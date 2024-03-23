831274e2916c26f41589d841362406a6d7283177
28206469m <suarezvaleroma@uvm.edu.ve>
2024-03-23 12:29:46 -0400

    Creamos vista principal de las vistas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  
+// Index
+router.get('/', async function(req, res, next) {
+  try {
+      res.render('index')
+  } catch (error) {
+      res.status(400).json({mensaje: error})
+  }
 });
 
 module.exports = router;
